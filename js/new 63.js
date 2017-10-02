42cb528aca890c645b79e99c8923d8b2915475d6
sirius16 <amozu16@gmail.com>
2017-10-01 23:01:49 -0400

    4J4T2

---
+++ js/new 63.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @name         KimCartoon Fetch Link
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://kimcartoon.me/Cartoon/*/*
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+(function () {
+
+	GM_setClipboard($kissenc.getSecretKey());
+	localStorage.secretkey = $kissenc.getSecretKey();
+	dispatchEvent(new Event("key"));
+	// $("body").keypress(e=>e.key=" " && obj($("#divDownload a").get().map(i=>i.href).join("\n")))
+})();
